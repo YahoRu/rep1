6e06cb61a5062f292a3ea774d9aa9b11106a2946
Yahor Ruban <rubanyahor@gmail.com>
2021-04-07 17:30:22 +0300

    'add day 8'

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,28 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long start = System.currentTimeMillis();
+        System.out.println(start);
+//
+//        String number = " ";
+//
+//        for (int i = 0; i <= 20000; i++) {
+//            number = number + i + " ";
+//        }
+//
+//        System.out.println(number);
+
+        StringBuilder sb = new StringBuilder(" ");
+
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb.toString());
+
+        long finish = System.currentTimeMillis();
+        System.out.println(finish);
+        long timeConsumedMillis = finish - start;
+        System.out.println(timeConsumedMillis);
     }
 }
